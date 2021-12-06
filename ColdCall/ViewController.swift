c8f684e882467c42b8169769c9ee7865d6cdb18e
albtoul <albtoul-kaseem@hotmail.com>
2021-12-06 23:43:18 +0300

    Finished Cold Call

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let names = ["Majed","Jomanah","Atheer","Albtoul","Basema"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func coldCallPressed(_ sender: UIButton) {
+        let random = Int.random(in: 0...names.count-1)
+        nameLabel.text = names[random]
+    }
+    
 }
 
